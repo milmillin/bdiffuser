8db6733bff4c6fe7ae2c4f7d6b10786b205707ea
Teerapat Jenrungrot <15001582+mjenrungrot@users.noreply.github.com>
2026-03-08 00:15:38 -0800

    fix(shared): export mission 32 schema type

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -2,6 +2,7 @@ export * from "./types.js";
 export * from "./protocol.js";
 export * from "./constants.js";
 export * from "./missionSchema.js";
+export * from "./missionSchemaTypes.js";
 export * from "./missions.js";
 export * from "./imageMap.js";
 export * from "./equipmentCardText.js";
